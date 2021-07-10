ad6cc7563cf09083d4cb63f6353991802206b4bd
Andrew Ma <anma@scires.com>
2021-07-10 00:32:20 -0500

    Fix pyinstaller command

--- build_executable/run_pyinstaller.bat
+++ build_executable/run_pyinstaller.bat
@@ -1,5 +1,9 @@
-pyinstaller -n %APP_NAME% --icon=sim_icon.ico --upx-dir=%UPX_PATH% --clean "../src/sim_csv_gui/app.py" --runtime-hook add_lib.py --noconsole -y --onedir
+:: GENERATE DIRECTORY FOR ZIP FILE
+pyinstaller -n "%APP_NAME%" --icon=sim_icon.ico --upx-dir="%UPX_PATH%" --clean "../src/sim_csv_gui/app.py" --runtime-hook add_lib.py --noconsole -y --onedir
 :: for DEBUG add '--noupx --debug bootloader'
+
+:: GENERATE SINGLE EXE FILE
+pyinstaller -n "%APP_NAME%" --icon=sim_icon.ico --upx-dir="%UPX_PATH%" --clean "../src/sim_csv_gui/app.py" --runtime-hook add_lib.py --noconsole -y --onefile
 
 del /s /q build
 rmdir /s /q build
