1dac4629529acdd8d4fb7661736b8f7752167c8d
Andrew Ma <anma@scires.com>
2021-07-31 16:04:48 -0500

    Modify filter script to do field checks before and after

--- filter_script.py
+++ filter_script.py
@@ -1,7 +1,7 @@
 import sys
 import csv
 from io import StringIO
-
+from sim_csv_script import check_that_field_is_valid
 
 INDEX_COLUMN_NAME = "FieldName"
 
@@ -14,6 +14,13 @@ def get_csv_dict(csv_string):
     csv_dict = {row[INDEX_COLUMN_NAME]: row for row in reader}
 
     return csv_dict
+
+
+def check_csv_dict(csv_dict):
+    for field_name, row_dict in csv_dict.items():
+        field_value = row_dict["FieldValue"]
+
+        check_that_field_is_valid(field_name, field_value)
 
 
 def get_str_from_csv_dict(csv_dict):
@@ -45,6 +52,9 @@ def main():
     # Convert CSV string to dictionary
     csv_dict = get_csv_dict(input_csv_string)
 
+    # Find invalid fields
+    check_csv_dict(csv_dict)
+
     #############################################################
     #####################  Modify CSV here ######################
 
@@ -52,6 +62,9 @@ def main():
     csv_dict["SPN"]["FieldValue"] = f'{csv_dict["SPN"]["FieldValue"][:-2]}{arg1[::-1]}'
 
     #############################################################
+
+    # Find invalid fields
+    check_csv_dict(csv_dict)
 
     # Convert CSV dictionary to string
     output_csv_string = get_str_from_csv_dict(csv_dict)
@@ -65,5 +78,9 @@ def main():
 
 
 if __name__ == "__main__":
-    # exit with the return code of main() function
-    sys.exit(main())
+    try:
+        # exit with the return code of main() function
+        sys.exit(main())
+    except Exception as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
